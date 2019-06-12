3e779f1b9e334dcc063f0f00b35576a4c4891e23
Stok <sean@stokedprojects.org>
2019-06-12 20:17:18 +0200

    index

--- download/arch_install.sh
+++ download/arch_install.sh
@@ -12,9 +12,9 @@ sgdisk -og $1
 sgdisk -n 1:2048:4095 -c 1:"bios" -t 1:ef02 $1
 sgdisk -n 2:4096:413695 -c 2:"boot" -t 2:8300 $1
 sgdisk -n 3:413696:8802303 -c 3:"root" -t 3:8300 $1
-sgdisk -n 3:8802304:10899455 -c 3:"swap" -t 3:8200 $1
+sgdisk -n 4:8802304:10899455 -c 4:"swap" -t 3:8200 $1
 ENDSECTOR=`sgdisk -E $1`
-sgdisk -n 4:10899456:$ENDSECTOR -c 4:"home" -t 4:8300 $1
+sgdisk -n 5:10899456:$ENDSECTOR -c 5:"home" -t 4:8300 $1
 sgdisk -p $1
 
 exit 0
