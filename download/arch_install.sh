f039d2a81a97d46366640dde4e05d5594bc6f96a
Stok <sean@stokedprojects.org>
2019-06-12 20:24:50 +0200

    next stages of install

--- download/arch_install.sh
+++ download/arch_install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+#Set timezone:
+timedatectl set-timezone Europe/Paris
+
 if [ -z "$1" ]
 then
     echo "No disk specified for install (typically use /dev/sda if virtualbox). Quitting."
@@ -16,5 +19,37 @@ sgdisk -n 4:8802304:10899455 -c 4:"swap" -t 3:8200 $1
 ENDSECTOR=`sgdisk -E $1`
 sgdisk -n 5:10899456:$ENDSECTOR -c 5:"home" -t 4:8300 $1
 sgdisk -p $1
+
+# formatting drive
+mkfs.ext4 /dev/sda2, then y
+
+# mount this new drive
+mount /dev/sda2 /mnt
+
+# Start install
+pacstrap /mnt base
+
+# Generating an fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# chroot in to new system
+arch-chroot /mnt
+
+# setting time
+ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+hwclock --systohc
+
+# set password
+passwd
+
+# Installing grub
+pacman –S grub os-prober
+
+# Then install
+grub-install $1
+grub-mkconfig -o /boot/grub/grub.cfg
+#(Select the disk, not the partition!)
+
+echo "Done. reboot drive by calling 'reboot'"
 
 exit 0
