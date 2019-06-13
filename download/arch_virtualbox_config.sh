04f242321d59bcc07ec7bd1b18a6ed4852dfe49a
Stok <sean@stokedprojects.org>
2019-06-13 21:19:38 +0200

    more variations to get fstab working

--- download/arch_virtualbox_config.sh
+++ download/arch_virtualbox_config.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+mount /dev/sda3 /mnt
+
 arch-chroot /mnt cp /etc/netctl/examples/ethernet-dhcp /etc/netctl/virtualbox-ethernet
 arch-chroot /mnt sed -i 's/eth0/enp0s3/g' /etc/netctl/virtualbox-ethernet
 arch-chroot /mnt sed -i 's/A basic dhcp ethernet connection/Enabling ethernet connection for virtualbox/g' /etc/netctl/virtualbox-ethernet
+
+umount /mnt
 echo "Once you've rebooted, remember to run: netctl start virtualbox-ethernet"
